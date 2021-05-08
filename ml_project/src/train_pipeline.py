4fb16f57a09e35d7665a671b52aba671f71045a3
Ekaterina Kasilina <abramova.katia@gmail.com>
2021-05-08 22:51:27 +0300

    test added

--- ml_project/src/train_pipeline.py
+++ ml_project/src/train_pipeline.py
@@ -35,8 +35,6 @@ def train_pipeline(
     transformer.fit(train_df)
 
     save_pkl_file(transformer, training_pipeline_params.path_config.output_transformer_path)
-
-
     train_features = pd.DataFrame(transformer.transform(train_df))
     train_target = get_target(train_df, training_pipeline_params.feature_params)
 
