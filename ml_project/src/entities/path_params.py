bab661c24f4a6489e90340f5638473ec4f1112e2
Ekaterina Kasilina <abramova.katia@gmail.com>
2021-05-23 16:47:41 +0300

    dir ml_project was removed

--- ml_project/src/entities/path_params.py
+++
@@ -1,9 +0,0 @@
-from dataclasses import dataclass
-
-
-@dataclass
-class PathParams:
-    input_data_path: str
-    output_model_path: str
-    output_transformer_path: str
-    metric_path: str
--- ml_project/src/utils/__init__.py
+++
@@ -1,8 +0,0 @@
-from .utils import load_pkl_file, save_metrics_to_json, save_pkl_file, read_data
-
-__all__ = [
-    "load_pkl_file",
-    "save_metrics_to_json",
-    "save_pkl_file",
-    "read_data"
-]
